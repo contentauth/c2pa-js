7e4834fdb7f5cc3db68bb156f85c2c9d20673637
Eli Mensch <mensch@adobe.com>
2025-08-12 10:22:03 -0400

    feat: Build testbed site

---
+++ tools/testbed/src/main.ts
@@ -0,0 +1,51 @@
+import { createC2pa } from '@contentauth/c2pa-web';
+import wasmSrc from '@contentauth/c2pa-wasm/assets/c2pa_bg.wasm?url';
+
+const c2pa = await createC2pa({ wasmSrc });
+
+const dropzone = document.getElementById('drop-zone');
+
+dropzone?.addEventListener('dragenter', () => {
+  dropzone.classList.add('active');
+});
+
+dropzone?.addEventListener('dragleave', () => {
+  dropzone.classList.remove('active');
+});
+
+dropzone?.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+dropzone?.addEventListener('drop', (e) => {
+  dropzone.classList.remove('active');
+
+  if (e.dataTransfer?.items) {
+    [...e.dataTransfer.items].forEach(async (item) => {
+      if (item.kind === 'file') {
+        const file = item.getAsFile();
+
+        if (!file) {
+          throw new Error('Could not get item as file');
+        }
+
+        try {
+          const start = performance.now();
+
+          const reader = await c2pa.reader.fromBlob(file.type, file);
+          const manifestStore = await reader.manifestStore();
+
+          const end = performance.now();
+
+          console.log(manifestStore);
+          console.log(`Took ${Math.round(end - start)}ms`);
+
+          await reader.free();
+        } catch (e) {
+          console.log('caught error', e);
+        }
+      }
+    });
+  }
+  e.preventDefault();
+});
---
+++ tools/testbed/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/tools/testbed',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
